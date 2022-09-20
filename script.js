678bd9d7028c4d6c2cf76f20e952cb3a612fca35
TermiNateVader <nathanbenstevens@gmail.com>
2022-09-20 00:07:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,53 @@ else {
 const topnav = document.getElementById("topnav");
 topnav.textContent = "TermiNateVader";
 
-
+window.addEventListener('load', function(){
+ 
+    var touchsurface = document.getElementById('touchsurface'),
+        startX,
+        startY,
+        dist,
+        threshold = 150, //required min distance traveled to be considered swipe
+        allowedTime = 200, // maximum time allowed to travel that distance
+        elapsedTime,
+        startTime
+ 
+    function handleswipe(isrightswipe){
+        if (isrightswipe){
+            plusDivs(-1)
+	}
+    }
+	 function handleswipe(isleftswipe){
+        if (isleftswipe){
+            plusDivs(1)
+	}
+    }
+ 
+    touchsurface.addEventListener('touchstart', function(e){
+        touchsurface.innerHTML = ''
+        var touchobj = e.changedTouches[0]
+        dist = 0
+        startX = touchobj.pageX
+        startY = touchobj.pageY
+        startTime = new Date().getTime() // record time when finger first makes contact with surface
+        e.preventDefault()
+    }, false)
+ 
+    touchsurface.addEventListener('touchmove', function(e){
+        e.preventDefault() // prevent scrolling when inside DIV
+    }, false)
+ 
+    touchsurface.addEventListener('touchend', function(e){
+        var touchobj = e.changedTouches[0]
+        dist = touchobj.pageX - startX // get total dist traveled by finger while in contact with surface
+        elapsedTime = new Date().getTime() - startTime // get time elapsed
+        // check that elapsed time is within specified, horizontal dist traveled >= threshold, and vertical dist traveled <= 100
+        var swiperightBol = (elapsedTime <= allowedTime && dist >= threshold && Math.abs(touchobj.pageY - startY) <= 100)
+        handleswipe(swiperightBol)
+        e.preventDefault()
+    }, false)
+ 
+}, false) // end window.onload
 			
 	
 var slideIndex = 1;
