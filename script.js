278f633c2754c9a2c19aff27b15b4e10c4fed534
TermiNateVader <nathanbenstevens@gmail.com>
2022-10-25 19:03:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -88,19 +88,13 @@ document.addEventListener('keydown', function(event) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-	var y = document.getElementsByClassName("blog");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
   x[slideIndex-1].style.display = "block"; 
-	if (n > y.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = y.length}
-  for (i = 0; i < y.length; i++) {
-    y[i].style.display = "none";  
-  }
-  y[slideIndex-1].style.display = "block";
+	
 }
 
 
